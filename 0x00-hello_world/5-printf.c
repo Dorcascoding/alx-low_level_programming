69e55c82bbbbb287fd2ad338278a1927bb41725e
Dorcascoding <dorcasodewole@gmail.com>
2023-02-17 02:04:06 +0100

    lol

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdin.h>
+/**
+* main -print a code line with puts function
+*
+* Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\b");
+return (0);
+}
