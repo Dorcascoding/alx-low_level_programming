6f7eec61d649017c8dfcd18106c4a5fec594b53a
Dorcascoding <dorcasodewole@gmail.com>
2023-02-18 02:25:30 +0100

    lol

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always (Success)
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch, <= 'z'; ch++)
+{putchar(ch);
+}
+putchar('\n');
+return (0);
+}
