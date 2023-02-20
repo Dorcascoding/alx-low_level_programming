af44dd94d9984f07d0ac8c56264a8edf079e2e6d
Dorcascoding <dorcasodewole@gmail.com>
2023-02-20 16:23:32 +0100

    he

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,28 @@
 
 int main(void)
 {
-int ch1, ch2;
-for (ch1 = 48; ch1 <= 56; ch1++)
+int ch1 = 48, ch2 = 49;
+for (; ch1 <= 56; ch1++)
 {
-	for (ch2 = 49; ch2 <= 57; ch2++)
+	for (; ch2 <= 57; ch2++)
 	{
-		putchar (ch1);
-		putchar (ch2);
-		if (ch1 != 56 || ch2 != 57)
+		if (ch1 == ch2)
 		{
-			putchar(44);
-			putchar(32);
+			continue;
 		}
+		else
+		{
+			putchar (ch1);
+			putchar (ch2);
+			if (ch1 != 56 || ch2 != 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
 	}
+	ch2 = ch1 + 1;
 }
 putchar('\n');
 return (0);
