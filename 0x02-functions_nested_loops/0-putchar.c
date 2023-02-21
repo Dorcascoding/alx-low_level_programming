a69ccd768e6573a4f9c7b56f87a0105b7b1af937
Dorcascoding <dorcasodewole@gmail.com>
2023-02-21 22:20:00 +0100

    me

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * main -Entry point
- * 
- * Return: Always 0
+ * main - main function
+ * _putchar: print the alphabets putchar
+ * Return: always 0
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,5 +16,4 @@ int main (void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-  
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,25 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 
-void print_alphabet(void);
-
-void print_alphabet_x10(void);
-
-int _islower(int cl);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int);
-
-int print_last_digit(int);
-
-void jack_bauer(void);
-
-int times_table(void);
-
-int add(int, int);
-
-void print_to_96(int n);
-
-void print_times_table(int n);
+#endif 
