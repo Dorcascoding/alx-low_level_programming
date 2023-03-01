7d249068169ed0dc741af2c7f9eb72a43b93da09
Dorcascoding <dorcasodewole@gmail.com>
2023-03-01 12:37:45 +0100

    101combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,19 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- *  * main - prints all possible different combinations of three digits
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 int n, m, l;
-for (n = 48; n < 58; n++)
-for (m = 49; m < 58; m++)
-for (l = 50; l < 58; l
-																					if (l > m && m > n
-putchar(n)
-putchar(m)
-putchar(l)
-if (n != 55 || m != 56
+for (n = 0; n < 8; n++)
+{
+for (m = n + 1;  m < 9; m++)
+{
+for (l = m + 1; l < 10; l++)
+{
+putchar ((n % 10) + '0');
+putchar ((m % 10) + '0');
+putchar ((l % 10) + '0');
+if (n == 7 && m ==9)
+	continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
