e5464da608cb4331e2d8ba216bb44d45715cbd29
Dorcascoding <dorcasodewole@gmail.com>
2023-03-30 01:04:55 +0100

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -19,7 +19,7 @@ va_start(ap, n);
   /* iterate through all parameter values*/
 for (param = 0; param < n; param++)
     /* get the next parameter value and add it to sum*/
-    sum += va_arg(ap, int);
+sum += va_arg(ap, int);
   /*Clean up*/
 va_end(ap);
 
